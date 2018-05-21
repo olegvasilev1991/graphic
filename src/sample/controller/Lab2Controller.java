9688f55cd7107e1d99973e69c60405e796d252e6
oleg.vasilev <oleg.vasilev1991@gmail.com>
2018-05-21 09:46:54 +0300

    fix

--- src/sample/controller/Lab2Controller.java
+++ src/sample/controller/Lab2Controller.java
@@ -1,8 +1,45 @@
 package sample.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class Lab2Controller {
-    private Lab1Controller LAB1;
-    public  Sample(Lab1Controller LAB1){
-        this.LAB1 = LAB1;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Button rotate;
+
+    @FXML
+    private Button imagelast;
+
+    @FXML
+    private Button gray;
+
+    @FXML
+    private Button gist;
+
+    @FXML
+    private ImageView cut;
+
+    @FXML
+    private Button curtimage;
+
+    @FXML
+    private Button loadimage;
+
+    @FXML
+    private Button menu;
+
+    @FXML
+    private Button point;
+    @FXML
+    void initialize() {
+
     }
+
 }
